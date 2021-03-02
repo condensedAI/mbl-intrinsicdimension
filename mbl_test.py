e9cb7a7cfa88c235319be5ef2926bad77dba0dea
toton-golio <qnf967@alumni.ku.dk>
2021-03-02 15:41:45 +0100

    ready for cluster

--- mbl_test.py
+++ mbl_test.py
@@ -5,45 +5,19 @@ for path in sys.path:
 sys.path.append('/abc')
 '''
 
-from mbl import *
+
 from datetime import datetime
 import matplotlib.pyplot as plt
-'''
-number = 5
-L = 4
-bitstring = binaryConvert(number, L)
-print(number, bitstring)
+from numpy import load
+from mbl import *
+buildnDiag(method ='dense', L_high=14, disorder_realizations=1000, disorders=20)
 
 
-ones = nOnes( bitstring)
-print('nOnes = ',  ones)
+#rs = rStatFromFiles(L_high = 14,disorder_realizations = 1000, disorders = 20)
 
+#print(rs)
+#print(np.shape(rs))
 
-for i in range(2,6):
-	print(binomial( i))
-'''
+#plotR(rs)
 
-#bs = basisStates(6)
-#print(bs)
-
-#ed = energyDiagonal()
-#print(ed)
-
-#H = constructHamiltonian(L=6, method='dense')
-#H1 = constructHamiltonian(L=6, method='sparse')
-#print(H_sparse)
-
-#H_dense = constructHamiltonianDense(L=6)
-#eig0 = diag(H)
-#eig1 = diag(H1)
-
-#eig1 = diagSparse(H_sparse)
-#print(H)
-#print(eig0)
-#print(eig1)
-#ls = levelSpacing(eig0) 
-#print(ls)
-
-benchmark(1000  ,8)
-
-#obtain_s_r(save=False)
+#print(load_eigs_npz('results/results-L-4-W-0.1-seed-1.npz'))
