a81e56aa02b34f0110f107ec92e29b4118efb3b5
toton-golio <qnf967@alumni.ku.dk>
2021-05-04 20:50:40 +0200

    minor edits

--- Cluster/launch.sh
+++ Cluster/launch.sh
@@ -2,7 +2,7 @@
 savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/results_NEW/"
 
 
-for i in `seq 0 999`
+for i in `seq 0 20`
 do
 	qsub -v output=$savedir,L=10,S=$i, W=[1.0, -N hubbard-L-10-S-$S controlscript.pbs
 done
