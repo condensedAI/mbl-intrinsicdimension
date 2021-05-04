6f0490cdc3334847e3ad71c67160eb0d161ce388
toton-golio <qnf967@alumni.ku.dk>
2021-05-04 20:19:42 +0200

    updated with suggested edits

--- Cluster/launch.sh
+++ Cluster/launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-savedir="/home/projects/ku_00067/scratch/ising/data/"
+savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/results_NEW/"
 
 
 for i in `seq 0 999`
 do
-	qsub -v output=$savedir,L=10,S=$i, W=[1.0, 1.44] -N Ising-L-10-T-$T controlscript.pbs
+	qsub -v output=$savedir,L=10,S=$i, W=[1.0, -N hubbard-L-10-S-$S controlscript.pbs
 done
