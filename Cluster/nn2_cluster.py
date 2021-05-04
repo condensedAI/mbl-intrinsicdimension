da091d93183b6cd85991c214e3022caf8aec26e1
toton-golio <qnf967@alumni.ku.dk>
2021-05-04 16:18:29 +0200

    2nn cleaned for cluster

---
+++ Cluster/nn2_cluster.py
@@ -0,0 +1,67 @@
+from numpy import load, array, zeros, log, vstack
+from numpy.linalg import lstsq
+from math import factorial
+from scipy.spatial import distance_matrix
+import time
+
+def load_eigs_npz(filename='data/results-L-12-W-1.0-seed-42.npz'):
+	data = load(filename)
+	eigvecs = data[data.files[1]]
+	return eigvecs.T
+
+def listFilenames(L = 10,
+				  ws = [1.0, 1.44, 1.89, 2.33, 2.78, 3.22, 3.67, 4.11, 4.56, 5.0, 
+						  5.56, 6.11, 6.67, 7.22, 7.78, 8.33, 8.89, 9.44],
+				   seed = 0):
+	files = ['data/results-L-{}-W-{}-seed-{}.npz'.format(L,round(w,2),seed) for w in ws]
+	
+	return files
+
+def load_many_eigs(filenames):
+	return array([load_eigs_npz(filename) for filename in filenames], dtype='object')
+
+
+def nn2(data):
+	'''
+	Find intrinsic dimension (ID) via 2-nearest-neighbours
+
+	https://www.nature.com/articles/s41598-017-11873-y
+	https://arxiv.org/pdf/2006.12953.pdf
+	_______________
+	Parameters:	eigvecs ---	Returns: Intrinsic Dimension
+	
+	'''
+	N = len(data)
+	dist_M = distance_matrix(data,data, p=1)
+	
+	# table of distances - state and \mu= r_2/r_1
+	mu = zeros((N,2))
+	for index, line in enumerate(dist_M):
+
+		r1, r2 = sorted(line)[1:3]
+		mu[index,0] = index+1
+		mu[index,1] = r2/r1
+		
+
+	#permutation function
+	sigma_i = dict(zip(range(1,len(mu)+1), array(sorted(mu, key=lambda x: x[1]))[:,0].astype(int)))
+	mu = dict(mu)
+	#cdf F(mu_{sigma(i)})
+	F_i = {}
+	for i in mu:
+		F_i[sigma_i[i]] = i/N
+
+	#fitting coordinates
+	x = log([mu[i] for i in sorted(mu.keys())])
+	y = array([1-F_i[i] for i in sorted(mu.keys())])
+
+	#avoid having log(0)
+	x = x[y>0]
+	y = y[y>0]
+
+	y = -1*log(y)
+
+	#fit line through origin to get the dimension
+	d = lstsq(vstack([x, zeros(len(x))]).T, y, rcond=None)[0][0]
+
+	return d
---
+++ Cluster/run.py
@@ -0,0 +1,32 @@
+from nn2_cluster import *
+import argparse
+
+
+def run(L, ws, seed,):
+    # List filenames
+    filenames = listFilenames(L=10,ws=[1.0,1.44], seed=0)
+
+    # Get eigencevtors
+    eigenvectors = load_many_eigs(filenames)
+
+    # Perform 2nn
+    IDs = [nn2(eigs) for eigs in eigenvectors]
+
+    #np.save(output + '/%d-%.5f' % (L, T), res, allow_pickle=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run 2NN on Hubbard chain Eigenvectors")
+    parser.add_argument('output', type=str, help='Output folder of the files')
+    parser.add_argument('-L', type=int, default=10, help='The system size (L)')
+    parser.add_argument('-W', type=list, default=20000, help='The disorders')
+    parser.add_argument('-S', type=int, help='The seed number')
+    args = parser.parse_args()
+
+    # Make sure the output directory exists
+    try:
+        os.makedirs(args.output, exist_ok = True)
+    except:
+        print("Directory '%s' could not be created" % args.output)
+
+    run(args.L, args.W, args.S)
