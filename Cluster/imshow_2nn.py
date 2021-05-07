4ea34b322b574f91dd8186553c8729aca7d4e811
Anton Gersvang Golles <antgol@g-12-l0002.cm.cluster>
2021-05-07 15:37:31 +0200

    a

--- Cluster/imshow_2nn.py
+++ Cluster/imshow_2nn.py
@@ -15,13 +15,21 @@ for seed in range(1000):
 data = np.array(data).T
 print(data)
 
-plt.figure(figsize=(10,2))
-plt.imshow(data)
-plt.xlabel('Seed number')
-plt.ylabel("disorder")
-plt.yticks(range(12),[1.0, 1.55, 2.09, 2.64, 3.18, 3.73, 4.27, 4.82, 5.36, 5.91, 6.45, 7.0])
-plt.title("nn2_output-L-{}-seeds-{}_1.png".format(L, seed+1))
-plt.savefig("nn2_output-L-{}-seeds-{}.png".format(L, seed+1), dpi=500)
+
+
+def forceAspect(ax,aspect):
+    im = ax.get_images()
+    extent =  im[0].get_extent()
+    ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.imshow(data, extent=[-1,1,-10,10])
+forceAspect(ax,aspect=0.5)
+
+fig.suptitle("nn2_output-L-{}-seeds-{}_1.png".format(L, seed+1), fontsize=16)
+fig.savefig("nn2_output-L-{}-seeds-{}.png".format(L, seed+1), dpi=500, bbox_inches='tight')
 
 
 x = [1.0, 1.55, 2.09, 2.64, 3.18, 3.73, 4.27, 4.82, 5.36, 5.91, 6.45, 7.0]
@@ -37,5 +45,6 @@ plt.title('Mean result from 2nn')
 plt.xlabel('disorder strength, $W$')
 plt.ylabel('Intrinsic dimension')
 
+plt.legend()
 
 plt.savefig('2nn_mean.png', dpi=500)
