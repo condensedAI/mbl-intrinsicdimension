92de39607927ed56b84aec13ec342e61ec48e903
toton-golio <qnf967@alumni.ku.dk>
2021-05-07 15:56:51 +0200

    a

--- Cluster/run.py
+++ Cluster/run.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def run(L, seed, output):
     # List filenames
-    filenames = listFilenames(L=12, seed=seed)
+    filenames = listFilenames(L=14, seed=seed)
 
     # Get eigencevtors
     eigenvectors = load_many_eigs(filenames)
