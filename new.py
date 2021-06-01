e7a1088fb694475f59b6764a99929fb3751b48b7
tonton-golio <antgol@g-12-l0002.cm.cluster>
2021-06-01 12:19:51 +0200

    works for r_stat :D

--- new.py
+++ new.py
@@ -1,25 +1,41 @@
 import numpy as np
 from utils import level_spaces, r_stat
 
-for first_seed in np.arange(0,500,100):
+L=10
+
+Ws = np.linspace(0.1,6.1,31)
+print(Ws)
+
+R_main = []
+R_std_main = []
+
+for first_seed in np.arange(0,10000,100):
 	last_seed = first_seed + 99
 
-	filename = '/home/projects/ku_00067/scratch/mbl-intrinsicdimension/fulldataset/L-8/eigvecs-and-eigvals-L-8-seeds-{}-{}.npy'.format(first_seed, last_seed)
+	filename = '/home/projects/ku_00067/scratch/mbl-intrinsicdimension/fulldataset/L-{}/eigvecs-and-eigvals-L-{}-seeds-{}-{}.npy'.format(L,L,first_seed, last_seed)
 	data = np.load(filename, allow_pickle=True)
 
-	first_seed, last_seed = int(filename.split('-')[8]), int(filename.split('-')[9].split('.')[0])
-
 	seeds = np.arange(first_seed, last_seed)
-	W = 0.1
 	vals_or_vecs = 'eigvals'
 
-	R = [[],[]] # mean and std
-	for seed in seeds:
-		eigs = data.item()[seed][W][vals_or_vecs]
-		r = r_stat(eigs)
-		R[0].append(np.mean(r))
-		R[1].append(np.std(r))
+	R = np.zeros((99,len(Ws)))
+	R_std = np.zeros((99,len(Ws)))
+	for index1, seed in enumerate(seeds):
+		for index2, W in enumerate(Ws):
+			W = round(W,1)
+			eigs = data.item()[seed][W][vals_or_vecs]
+			r = r_stat(eigs)
+			R[index1,index2] = np.mean(r)
+			R_std[index1,index2] = np.std(r)
 
 
-	print(np.mean(R, axis=1))
-
+	R_main.append(np.mean(R,axis=0))
+	R_std_main.append(np.mean(R_std,axis=0))	
+	
+	
+R, std = np.array(R_main), np.array(R_std_main)
+#print(np.shape(R))
+#print(np.shape(std))
+#print(R)
+print(np.mean(R, axis=0))
+print(np.mean(std, axis=0))
